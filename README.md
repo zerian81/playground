0ddafc5b0526722794ffdd7bde8c97b152bc5eca
Dan Agar <zerian81@users.noreply.github.com>
2016-04-10 20:56:27 -0400

    Removed unnecessary function from clojure-primes.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # playground
-My random playground
+This is where I put stuff that I'm learning about, tinkering with, whatever. Don't expect much order here - it's pretty chaotic!
--- clojure-primes/src/primes/core.clj
+++ clojure-primes/src/primes/core.clj
@@ -6,16 +6,12 @@
 	[n numberList]
 	(filter #(not (zero? (mod % n))) numberList))
 
-(defn appendAll [l1 l2]
-	"Append one list to another"
-	(if (empty? l2) l1 (recur (conj l1 (first l2)) (rest l2))))
-
 (defn sieve
 	"Generate a list of primes using the sieve of eratosthenes algorithm"
 	[n]
 	(loop [primes [] candidates (range 2 (inc n))] 
 		(if (>= (first candidates) (Math/sqrt (last candidates))) 
-			(appendAll primes candidates)
+			(concat primes candidates)
 			(recur (conj primes (first candidates)) (filterMultiplesOf (first candidates) (rest candidates))) 
 			)))
 
